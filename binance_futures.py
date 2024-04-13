9c03ba362aaba2e9d2d7e2a8c31fc6ccfc982b61
Kundan Paudel <paudelkundan@gmail.com>
2024-04-13 15:50:21 -0600

    working on labels, not displaying for some reason

--- binance_futures.py
+++ binance_futures.py
@@ -1,6 +1,15 @@
 import logging
+import requests
+import pprint
 
 logger = logging.getLogger()
 
-def write_log():
-    logger.info("Binance Connector Connected")
+def get_contracts():
+    response_object = requests.get("https://testnet.binancefuture.com/fapi/v1/exchangeInfo")
+    print(response_object.status_code)
+    contracts = []
+    for contract in response_object.json()['symbols']:
+        print(contract['pair'])
+    return contracts
+
+print(get_contracts())
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 ## Imports
-
 ## UI import
 import tkinter as tk 
+import tkmacosx as tkm
+from tkinter import Label
+# from tkmacosx import Label
+
 ## logger import
 import logging
+from bitmex import get_contracts
 
 logger = logging.getLogger()
 
@@ -22,8 +26,15 @@ logger.addHandler(file_handler)
 
 ## Following only works if main.py is executed.
 if __name__ == '__main__':
+    bitmex_contracts = get_contracts()
+
     ## Main window of application.
-    root = tk.Tk() 
+    root = tk.Tk()
+
+    for contract in bitmex_contracts:
+        label_widget = Label(root, text="hello")
+        label_widget.pack(side = tk.TOP)
+
     ## Function that keeps the window open indefinitely 
     # until any user input is given.
     root.mainloop()
